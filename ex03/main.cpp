cf85f22bfe76c9f22884f23be53b66423e7d0428
Omisev <verissimo.r@gmail.com>
2024-04-09 20:05:42 +0200

    Couleur de displayStatus \n\nModification de la couleur de displayStatus en bleu

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: raveriss <raveriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 19:41:57 by raveriss          #+#    #+#             */
-/*   Updated: 2024/04/09 16:18:28 by raveriss         ###   ########.fr       */
+/*   Updated: 2024/04/09 20:03:32 by raveriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,21 @@
 #include <iostream>
 
 void displayStatus(const std::string& name, int hitPoints, int energyPoints) {
-    std::cout << name << " has " << hitPoints << " hit points and "
-              << energyPoints << " energy points left." << std::endl;
+    std::cout << "\033[34m" <<  name << " has " << hitPoints << " hit points and "
+              << energyPoints << " energy points left." << std::endl << std::endl << "\033[0m";
 }
 
 int main() {
     DiamondTrap dt("DiamondBoy");
+    displayStatus(dt.getName(), dt.getHitPoints(), dt.getEnergyPoints());
 
     dt.whoAmI();
     dt.attack("Enemy");
+    displayStatus(dt.getName(), dt.getHitPoints(), dt.getEnergyPoints());
     dt.takeDamage(30);
+    displayStatus(dt.getName(), dt.getHitPoints(), dt.getEnergyPoints());
     dt.beRepaired(20);
+    displayStatus(dt.getName(), dt.getHitPoints(), dt.getEnergyPoints());
 
     return 0;
 }
